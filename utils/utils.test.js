178c8a4f5282daf06c9ad789feb59eb7266d2602
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-19 09:08:28 +0100

    test(utils/utils.test.js) setName test
    Test for setName
    
    Section 6, Lesson 53

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -5,7 +5,20 @@ it('Should add two numbers', () => {
   const res = utils.add(33, 11);
   expect(res).toBe(44).toBeA('number');
 });
+
 it('Should square 2', () => {
   const res = utils.square(2);
   expect(res).toBe(4).toBeA('number');
+});
+
+it('Should expect some values', () => {
+  expect(12).toNotBe(11);
+  expect({ name: 'andrew' }).toNotEqual({ name: 'Andrew' });
+  expect([2, 3, 4]).toExclude(1);
+  expect({ name: 'Alberto', age: 32, location: 'Madrid' }).toInclude({age: 32}).toExclude({age: 31});
+});
+
+it('Should include the names in the object', () => {
+  const user = utils.setName({}, 'Alberto Eyo');
+  expect(user).toInclude({firstName: 'Alberto', lastName: 'Eyo'});
 });
