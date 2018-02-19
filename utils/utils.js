410c65a7cece4a8bc68b0d2ea63e9d65fa51e9a1
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-19 09:07:43 +0100

    feat(utils/utils.js) New methods
    Include new methods for testing
    
    Section 6, Lesson 53

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,16 @@
 module.exports = {
   add: (a, b) => a + b,
-  square: x => x * x
+  square: x => x * x,
+  setName: (user, fullName) => {
+    // Set first name and last Nama
+    const outUser = Object.assign({}, user);
+    fullName.split(' ').map((value, index) => {
+      if (index === 0) {
+        outUser.firstName = value;
+      } else {
+        outUser.lastName = value;
+      }
+    });
+    return outUser;
+  }
 };
