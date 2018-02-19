96f072b62af2647ad1ab1362cb9b2f20f21207f0
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-19 11:48:58 +0100

    test() Describing tests
    Nesting tests under describe definitions
    
    S6,L57

--- server/server.test.js
+++ server/server.test.js
@@ -2,19 +2,23 @@ const request = require('supertest');
 const app = require('./server').app;
 const expect = require('expect');
 /* global define, it, describe, before, beforeEach, afterEach, after */
-
-it('Should return hello world response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect(res => expect(res.body).toInclude({error: 'Page not found.', name: 'Todo App v1.0'}))
-    .end(done);
-});
-
-it('Should the users get return the myself user', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect(res => expect(res.body).toInclude({name: 'Alberto', age: 32}))
-    .end(done);
+describe('Server', () => {
+  describe('#GET /', () => {
+    it('Should return hello world response', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect(res => expect(res.body).toInclude({error: 'Page not found.', name: 'Todo App v1.0'}))
+        .end(done);
+    });
+  });
+  describe('#GET /users', () => {
+    it('Should the users get return the myself user', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect(res => expect(res.body).toInclude({name: 'Alberto', age: 32}))
+        .end(done);
+    });
+  });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -2,38 +2,38 @@ const expect = require('expect');
 const utils = require('./utils');
 
 /* global define, it, describe, before, beforeEach, afterEach, after */
-it('Should add two numbers', () => {
-  const res = utils.add(33, 11);
-  expect(res).toBe(44).toBeA('number');
-});
 
-it('Should square 2', () => {
-  const res = utils.square(2);
-  expect(res).toBe(4).toBeA('number');
-});
-
-it('Should expect some values', () => {
-  expect(12).toNotBe(11);
-  expect({ name: 'andrew' }).toNotEqual({ name: 'Andrew' });
-  expect([2, 3, 4]).toExclude(1);
-  expect({ name: 'Alberto', age: 32, location: 'Madrid' }).toInclude({age: 32}).toExclude({age: 31});
-});
-
-it('Should include the names in the object', () => {
-  const user = utils.setName({}, 'Alberto Eyo');
-  expect(user).toInclude({firstName: 'Alberto', lastName: 'Eyo'});
-});
-
-it('Should add aync two numbers', (done) => {
-  utils.asyncAdd(4, 5, (sum) => {
-    expect(sum).toBe(9).toBeA('number');
-    done();
+describe('Utils', () => {
+  describe('#add', () => {
+    it('Should add two numbers', () => {
+      const res = utils.add(33, 11);
+      expect(res).toBe(44).toBeA('number');
+    });
+    it('Should add aync two numbers', (done) => {
+      utils.asyncAdd(4, 5, (sum) => {
+        expect(sum).toBe(9).toBeA('number');
+        done();
+      });
+    });
   });
-});
-
-it('Should square async 12 resulting 144', (done) => {
-  utils.asyncSquare(12, (square) => {
-    expect(square).toBe(144).toBeA('number');
-    done();
+  it('Should square 2', () => {
+    const res = utils.square(2);
+    expect(res).toBe(4).toBeA('number');
+  });
+  it('Should expect some values', () => {
+    expect(12).toNotBe(11);
+    expect({ name: 'andrew' }).toNotEqual({ name: 'Andrew' });
+    expect([2, 3, 4]).toExclude(1);
+    expect({ name: 'Alberto', age: 32, location: 'Madrid' }).toInclude({age: 32}).toExclude({age: 31});
+  });
+  it('Should include the names in the object', () => {
+    const user = utils.setName({}, 'Alberto Eyo');
+    expect(user).toInclude({firstName: 'Alberto', lastName: 'Eyo'});
+  });
+  it('Should square async 12 resulting 144', (done) => {
+    utils.asyncSquare(12, (square) => {
+      expect(square).toBe(144).toBeA('number');
+      done();
+    });
   });
 });
