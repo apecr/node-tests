46a0ae17bd9e63f57ee2196db76fc632b54386a3
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-19 11:40:12 +0100

    test() Testing app
    Testing an express app with supertest
    
    S6,L56

--- server/server.js
+++ server/server.js
@@ -3,7 +3,16 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  });
+});
+
+app.get('/users', (req, res) => {
+  res.status(200).send([{name: 'Oscar', age: 43}, {name: 'Alberto', age: 32}, {name: 'Nacho', age: 35}]);
 });
 
 app.listen(3000);
+
+module.exports.app = app;
