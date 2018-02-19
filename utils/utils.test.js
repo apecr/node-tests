8fa5cc8169af4af50d99af8b7786ec839b78270b
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-02-19 08:51:57 +0100

    test(utils/utils.test.js) Include expect
    Include expects for the tests
    
    Section 6, Lesson 52

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,14 +1,11 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('Should add two numbers', () => {
   const res = utils.add(33, 11);
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+  expect(res).toBe(44).toBeA('number');
 });
 it('Should square 2', () => {
   const res = utils.square(2);
-  if (res !== 4) {
-    throw new Error(`Expected 4, but got ${res}`);
-  }
+  expect(res).toBe(4).toBeA('number');
 });
